5d236d6a58033cf90e45e50410e1d61d94a1246a
menulln <39300523+menulln@users.noreply.github.com>
2022-11-09 16:30:18 +0100

    Add function operate()

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,20 @@
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            substract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
